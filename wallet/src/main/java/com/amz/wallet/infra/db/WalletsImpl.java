7afdf6bfdcb27f813ecff923e8ea34043840b7e0
ali <ail77erfqagh99@gmail.com>
2026-06-06 22:32:01 +0330

    refactor: add mapper

--- wallet/src/main/java/com/amz/wallet/infra/db/WalletsImpl.java
+++ wallet/src/main/java/com/amz/wallet/infra/db/WalletsImpl.java
@@ -3,12 +3,9 @@ package com.amz.wallet.infra.db;
 import com.amz.wallet.domain.Wallet;
 import com.amz.wallet.domain.Wallets;
 import com.amz.wallet.infra.db.mapper.WalletMapper;
-import com.amz.wallet.persentation.dto.Currency;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
-import java.util.Map;
 import java.util.Optional;
 
 @Repository
@@ -21,9 +18,7 @@ public class WalletsImpl implements Wallets {
 
     @Override
     public Optional<Wallet> getById(long id) {
-        repository.findById(id)
-                .map(walletEntity -> new Wallet());
-
+        return repository.findById(id).map(walletMapper::toDomain);
     }
 
     @Override
--- wallet/src/main/java/com/amz/wallet/infra/db/mapper/WalletMapper.java
+++ wallet/src/main/java/com/amz/wallet/infra/db/mapper/WalletMapper.java
@@ -1,11 +1,45 @@
 package com.amz.wallet.infra.db.mapper;
 
 import com.amz.wallet.domain.Wallet;
+import com.amz.wallet.infra.db.CurrencyAmountEntity;
 import com.amz.wallet.infra.db.WalletEntity;
+import com.amz.wallet.persentation.dto.Currency;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper
 public interface WalletMapper {
 
-    WalletEntity toJpaEntity(Wallet wallet);
+    @Mapping(target = "amountBaseOnCurrency", source = "currencies")
+    Wallet toDomain(WalletEntity entity);
+
+    @Mapping(target = "currencies", source = "amountBaseOnCurrency")
+    WalletEntity toEntity(Wallet wallet);
+
+    default Map<Currency, BigDecimal> mapCurrenciesToMap(Set<CurrencyAmountEntity> currencies) {
+        if (currencies == null) {
+            return null;
+        }
+        return currencies.stream().collect(Collectors.toMap(
+                        CurrencyAmountEntity::getCurrency,
+                        CurrencyAmountEntity::getAmount));
+    }
+
+    default Set<CurrencyAmountEntity> mapMapToCurrencies(Map<Currency, BigDecimal> map) {
+        if (map == null) {
+            return null;
+        }
+        return map.entrySet().stream()
+                .map(entry -> {
+                    var entity = new CurrencyAmountEntity();
+                    entity.setCurrency(entry.getKey());
+                    entity.setAmount(entry.getValue());
+                    return entity;}).collect(Collectors.toSet());
+    }
+
 }
